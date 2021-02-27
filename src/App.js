b19dbc48cec87ab940a75daad0c91546ff6f825e
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-02-27 22:32:25 +0530

    Inject data using curly Braces

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 class App extends Component{
   render() {
+    let x=1;
+    let y=2;
     return (
       <div>
-        Phone Directory
+        {/* <div className="header">
+          Phone Directory
+        </div>
+        <button> Add </button>
+        <div>
+          <span>Name</span> <br/>
+          <span>Phone</span>
+        </div> */}
+        <label htmlFor="name">Name:</label>
+        <input type="text" defaultValue={x+y} placeholder="Enter here" />
+        
       </div>
     );
   }
