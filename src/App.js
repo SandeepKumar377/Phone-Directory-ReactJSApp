e220a7a406110bc4feb6963417112f0513ba643e
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-03-01 01:13:42 +0530

    add header using component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
+import Header from "./Header"
 
 class App extends Component{
   render() {
-    let x=1;
-    let y=2;
     return (
       <div>
-        {/* <div className="header">
-          Phone Directory
-        </div>
+        <Header/>
         <button> Add </button>
         <div>
           <span>Name</span> <br/>
           <span>Phone</span>
-        </div> */}
-        <label htmlFor="name">Name:</label>
-        <input type="text" defaultValue={x+y} placeholder="Enter here" />
-        
-      </div>
+        </div>
+        </div>
     );
   }
 }
