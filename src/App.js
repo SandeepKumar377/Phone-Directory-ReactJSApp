732afcfd7eb0460dafe7b6909a88e404f2eb37f2
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-03-03 22:06:21 +0530

    Pass heading of Header as props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   render() {
     return (
       <div>
-        <Header/>
+        <Header heading="Phone Directory App"/>
         <PhoneDirectoryDetails/>
       </div>
     );
