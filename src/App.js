0c34595a092519407dea44dc779e04046a53923e
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-03-04 11:01:10 +0530

    add 'Subscriber component' and change file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 import Header from "./Header"
-import PhoneDirectoryDetails from './PhoneDirectoryDetails';
+import './App.css'
 
 class App extends Component{
+
+ deleteHandler(){
+    alert("Delete Clicked");
+}
   render() {
+    let subscribers=[
+      {
+          id:1,
+          name:"Sandeep",
+          phone:"5555555"
+      },
+      {
+          id:2,
+          name:"Sandy",
+          phone:"58852555"
+      }
+  ]
     return (
       <div>
         <Header heading="Phone Directory App"/>
-        <PhoneDirectoryDetails/>
+        <div className="component-body-container">
+            <button className="custom-btn add-btn"> Add </button>
+            <div className="grid-container heading-container">
+                <span className="grid-item name-heading">NAME</span>
+                <span className="grid-item phone-heading">PHONE</span>
+            </div>  
+            {
+                subscribers.map(sub=> {
+                    return <div key={sub.id} className="grid-container">
+                        <span className="grid-item">{sub.name}</span>
+                        <span className="grid-item">{sub.phone}</span>
+                        <button className="custom-btn delete-btn" onClick={this.deleteHandler}> DELETE </button>                       
+                    </div>                    
+                })
+            }
+                       
+        </div>
       </div>
     );
   }
